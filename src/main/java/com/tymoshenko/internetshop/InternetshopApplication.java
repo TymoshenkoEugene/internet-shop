b7d641466c36f7be824418dff136c58436d613ac
TymoshenkoEugene <tymoshenkoeugene@gmail.com>
2017-08-29 11:39:54 +0300

    First commit

---
+++ src/main/java/com/tymoshenko/internetshop/InternetshopApplication.java
@@ -0,0 +1,12 @@
+package com.tymoshenko.internetshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternetshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternetshopApplication.class, args);
+	}
+}
